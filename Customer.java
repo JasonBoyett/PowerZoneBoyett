8dfc19c5e642e7ad83fb3a44481c12582159c558
JasonBoyett <jason.boyett@gmail.com>
2022-09-01 01:13:02 -0500

    chasing down null bug

--- Customer.java
+++ Customer.java
@@ -1,42 +1,44 @@
 public class Customer {
     private String customerID;
-    
+
     public String getCustomerID() {
         return customerID;
     }
 
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
     public boolean validateCustomerID(String customerID) {
-        for(int i = 0; i < customerID.length(); i++) {
-            if( customerID.charAt(i) >= 'A' || customerID.charAt(i) <= 'Z'){
-                
-            } 
-            if( customerID.charAt(i) >= 'a' || customerID.charAt(i) <= 'z'){
-                
-            } 
-            if( customerID.charAt(i) >= '0' || customerID.charAt(i) <= '9'){
-                
-            } 
-            else{
+        for (int i = 0; i < this.customerID.length(); i++) {
+            if (this.customerID.charAt(i) >= 'A' || this.customerID.charAt(i) <= 'Z') {
+
+            }
+            if (this.customerID.charAt(i) >= 'a' || this.customerID.charAt(i) <= 'z') {
+
+            }
+            if (this.customerID.charAt(i) >= '0' || this.customerID.charAt(i) <= '9') {
+
+            } else {
                 return false;
             }
         }
         return true;
     }
 
-    private void formatCustomerID(String customerID){
+    private void formatCustomerID(String customerID) {
         this.customerID = this.customerID.toUpperCase();
-        this.customerID = this.customerID.charAt(0) + "-" + this.customerID.substring(1, 3) + "-" + this.customerID.charAt(4);
+        this.customerID = this.customerID.charAt(0) + "-" + this.customerID.substring(1, 3) + "-"
+                + this.customerID.charAt(4);
     }
-        
 
     public Customer(String customerID) {
         this.customerID = customerID;
-        if(this.validateCustomerID(this.customerID) == false){
+        if (this.validateCustomerID(this.customerID) == false) {
             ErrorGUI error = new ErrorGUI();
             error.setVisible(true);
-        }
-        else
-        this.formatCustomerID(this.customerID);
+        } else
+            this.formatCustomerID(this.customerID);
     }
 
 }
--- OpenerGUI.java
+++ OpenerGUI.java
@@ -1,24 +1,27 @@
-import java.io.File;
-
+import java.io.*;
+import java.awt.*;
+import java.awt.Image;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
+import java.awt.Toolkit;
 
-
-public class OpenerGUI extends JFrame{
+public class OpenerGUI extends JFrame {
     String customerID;
     int timeBooked;
     BookButton bookButton;
+    Image background;
 
-    public OpenerGUI(){
-        //bookButton = new BookButton(this.customerID,this);
-        new ImageIcon background = createImage("PowerZoneOpenerScreen.png"); 
-
+    public OpenerGUI() {
+        bookButton = new BookButton(this.customerID, this);
+        final ImageIcon backgroundIcon = new ImageIcon("PowerZoneOpenerScreen.png");
+        Image backgroundImage = backgroundIcon.getImage();
         this.setVisible(true);
-        this.setSize(1280,720);
-        this.setContentPane();
-        
+        this.setSize(1280, 720);
+        this.setIconImage(backgroundImage);
+
     }
 }
