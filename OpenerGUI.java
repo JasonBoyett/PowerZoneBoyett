20f1ce41c0532fc922a3b93d8bc54b6f476a76bb
JasonBoyett <93905215+JasonBoyett@users.noreply.github.com>
2022-08-31 19:40:52 -0500

    setting up background

--- OpenerGUI.java
+++ OpenerGUI.java
@@ -1,7 +1,24 @@
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
 
 public class OpenerGUI extends JFrame{
     String customerID;
     int timeBooked;
     BookButton bookButton;
+
+    public OpenerGUI(){
+        //bookButton = new BookButton(this.customerID,this);
+        new ImageIcon background = createImage("PowerZoneOpenerScreen.png"); 
+
+        this.setVisible(true);
+        this.setSize(1280,720);
+        this.setContentPane();
+        
+    }
 }
