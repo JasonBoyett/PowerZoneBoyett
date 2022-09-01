54e53e18c546f4d80d98f846317705344e32b848
JasonBoyett <jason.boyett@gmail.com>
2022-09-01 01:36:38 -0500

    found and fixed null object bug

--- OpenerGUI.java
+++ OpenerGUI.java
@@ -10,18 +10,17 @@ import javax.swing.JPanel;
 import java.awt.Toolkit;
 
 public class OpenerGUI extends JFrame {
-    String customerID;
+    String customerID = "#####";
     int timeBooked;
-    BookButton bookButton;
     Image background;
 
     public OpenerGUI() {
-        bookButton = new BookButton(this.customerID, this);
+        BookButton bookButton = new BookButton(this.customerID, this);
         final ImageIcon backgroundIcon = new ImageIcon("PowerZoneOpenerScreen.png");
         Image backgroundImage = backgroundIcon.getImage();
         this.setVisible(true);
         this.setSize(1280, 720);
         this.setIconImage(backgroundImage);
-
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
